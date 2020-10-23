3368c048f12fae5ec782e70f9ed452641a847c38
coco369 <1571236356@qq.com>
2020-10-23 10:51:55 +0800

    百度转高德坐标

---
+++ coord_convert/bd2gcj.py
@@ -0,0 +1,58 @@
+"""
+    百度坐标系(BD-09)转火星坐标系(GCJ-02)
+    百度——>谷歌、高德
+"""
+import math
+
+
+class BdToGD():
+
+    def __init__(self):
+        self.x_pi = 3.14159265358979324 * 3000.0 / 180.0
+        self.pi = 3.1415926535897932384626  # π
+        self.a = 6378245.0  # 长半轴
+        self.ee = 0.00669342162296594323  # 偏心率平方
+        self.coordinate = []
+        self.lng = []
+        self.lat = []
+        self.converted_lng = []
+        self.converted_lat = []
+
+    def transformlat(self, lng, lat):
+        ret = -100.0 + 2.0 * lng + 3.0 * lat + 0.2 * lat * lat + \
+              0.1 * lng * lat + 0.2 * math.sqrt(math.fabs(lng))
+        ret += (20.0 * math.sin(6.0 * lng * self.pi) + 20.0 *
+                math.sin(2.0 * lng * self.pi)) * 2.0 / 3.0
+        ret += (20.0 * math.sin(lat * self.pi) + 40.0 *
+                math.sin(lat / 3.0 * self.pi)) * 2.0 / 3.0
+        ret += (160.0 * math.sin(lat / 12.0 * self.pi) + 320 *
+                math.sin(lat * self.pi / 30.0)) * 2.0 / 3.0
+        return ret
+
+    def transformlng(self, lng, lat):
+        ret = 300.0 + lng + 2.0 * lat + 0.1 * lng * lng + \
+              0.1 * lng * lat + 0.1 * math.sqrt(math.fabs(lng))
+        ret += (20.0 * math.sin(6.0 * lng * self.pi) + 20.0 *
+                math.sin(2.0 * lng * self.pi)) * 2.0 / 3.0
+        ret += (20.0 * math.sin(lng * self.pi) + 40.0 *
+                math.sin(lng / 3.0 * self.pi)) * 2.0 / 3.0
+        ret += (150.0 * math.sin(lng / 12.0 * self.pi) + 300.0 *
+                math.sin(lng / 30.0 * self.pi)) * 2.0 / 3.0
+        return ret
+
+    def bdToGaoDe(self, lng, lat):
+        """
+            百度坐标转高德坐标
+        """
+        dlat = self.transformlat(lng - 105.0, lat - 35.0)
+
+        dlng = self.transformlng(lng - 105.0, lat - 35.0)
+        radlat = lat / 180.0 * self.pi
+        magic = math.sin(radlat)
+        magic = 1 - self.ee * magic * magic
+        sqrtmagic = math.sqrt(magic)
+        dlat = (dlat * 180.0) / ((self.a * (1 - self.ee)) / (magic * sqrtmagic) * self.pi)
+        dlng = (dlng * 180.0) / (self.a / sqrtmagic * math.cos(radlat) * self.pi)
+        mglat = lat + dlat
+        mglng = lng + dlng
+        return [mglng, mglat]
